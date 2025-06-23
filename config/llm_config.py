fa6b70c850c813ea1c05a2b21726f6f9ac54c780
Abhi-blue-alegator <abhishek.a.delta@gmail.com>
2025-06-23 14:59:02 +0530

    api-key configured

--- config/llm_config.py
+++ config/llm_config.py
@@ -1,9 +1,16 @@
 # config/llm_config.py
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 import lancedb
+from dotenv import load_dotenv
+import os
+
 
 BATCH_SIZE = 100
 EMBEDDING_MODEL = "text-embedding-3-small"
+
+
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
 
 llm = ChatOpenAI(model="gpt-4o-mini")
 embeddings = OpenAIEmbeddings(model=EMBEDDING_MODEL, chunk_size=BATCH_SIZE)
--- config/settings.py
+++ config/settings.py
@@ -3,3 +3,5 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+
+api_key = os.getenv("OPENAI_API_KEY")
